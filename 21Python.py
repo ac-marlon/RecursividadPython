3a8342d73386b863cee169b70c798ae395fe0fe7
ac-marlon <acmarlon@outlook.com>
2017-08-17 13:36:27 -0500

    Validación de letras por valor de 10

--- 21Python.py
+++ 21Python.py
@@ -5,25 +5,71 @@ def generar_mazo():
     return sample([(x, y) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] for y in ['p', 't', 'd', 'c']], 52)
 
 
-def verificar_veintiuna(mano):
-    if mano[0][0] + mano[1][0]<21:
-        print 'Holiiiiiiii!!!'
+def valor_mano(mano):
+    if mano[0][0] == 'J':
+        if mano[1][0] == 'J':
+            return 20
+        elif mano[1][0] == 'Q':
+            return 20
+        elif mano[1][0] == 'K':
+            return 20
+        return 10 + mano[1][0]
+    elif mano[0][0] == 'Q':
+        if mano[1][0] == 'J':
+            return 20
+        elif mano[1][0] == 'Q':
+            return 20
+        elif mano[1][0] == 'K':
+            return 20
+        return 10 + mano[1][0]
+    elif mano[0][0] == 'K':
+        if mano[1][0] == 'J':
+            return 20
+        elif mano[1][0] == 'Q':
+            return 20
+        elif mano[1][0] == 'K':
+            return 20
+        return 10 + mano[1][0]
+
+    if mano[1][0] == 'J':
+        if mano[0][0] == 'J':
+            return 20
+        elif mano[0][0] == 'Q':
+            return 20
+        elif mano[0][0] == 'K':
+            return 20
+        return 10 + mano[0][0]
+    elif mano[1][0] == 'Q':
+        if mano[0][0] == 'J':
+            return 20
+        elif mano[0][0] == 'Q':
+            return 20
+        elif mano[0][0] == 'K':
+            return 20
+        return 10 + mano[0][0]
+    elif mano[1][0] == 'K':
+        if mano[0][0] == 'J':
+            return 20
+        elif mano[0][0] == 'Q':
+            return 20
+        elif mano[0][0] == 'K':
+            return 20
+        return 10 + mano[0][0]
+
+    else:
+        return mano[0][0] + mano[1][0]
 
 
-def jugar(mazo):
+def jugar(mazo, casa, jugador):
+    print casa
+    print jugador
     if mazo != []:
-        print mazo[0:2]
-        verificar_veintiuna(mazo[0:2])
-        # verificar_veintiuna(mazo[1:2])
-        return jugar(mazo[50:])
-        
+        if casa == []:
+            return jugar(mazo[4:], casa + mazo[:2], jugador + mazo[2:4])
+        if valor_mano(casa) < 21 and valor_mano(jugador) < 21:
+            print valor_mano(casa)
+            print valor_mano(jugador)
+            return jugar(mazo[2:], casa + [mazo[0]], jugador + [mazo[1]])
 
-jugar(generar_mazo())
 
-
-def pedir_carta(mazo):
-    print mazo[:3]
-    return jugar(mazo[49:])
-    
-
-# pedir_carta(generar_mazo())
+jugar(generar_mazo(), [], [])
