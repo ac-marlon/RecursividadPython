dc7af9fa7dd92b0f1c00762531fbe0faf279f825
ac-marlon <acmarlon@outlook.com>
2017-08-22 13:45:42 -0500

    Update 21Python.py

--- 21Python.py
+++ 21Python.py
@@ -2,74 +2,77 @@ from random import *
 
 
 def generar_mazo():
-    return sample([(x, y) for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] for y in ['p', 't', 'd', 'c']], 52)
+    return sample([(x, y) for x in ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] for y in ['p', 't', 'd', 'c']], 52)
+
+
+def valor_carta(carta):
+    if str(carta[0]) in "JQK":
+        return 10
+    if str(carta[0]) == "A":
+        return 1
+    return carta[0]
 
 
 def valor_mano(mano):
-    if mano[0][0] == 'J':
-        if mano[1][0] == 'J':
-            return 20
-        elif mano[1][0] == 'Q':
-            return 20
-        elif mano[1][0] == 'K':
-            return 20
-        return 10 + mano[1][0]
-    elif mano[0][0] == 'Q':
-        if mano[1][0] == 'J':
-            return 20
-        elif mano[1][0] == 'Q':
-            return 20
-        elif mano[1][0] == 'K':
-            return 20
-        return 10 + mano[1][0]
-    elif mano[0][0] == 'K':
-        if mano[1][0] == 'J':
-            return 20
-        elif mano[1][0] == 'Q':
-            return 20
-        elif mano[1][0] == 'K':
-            return 20
-        return 10 + mano[1][0]
-
-    if mano[1][0] == 'J':
-        if mano[0][0] == 'J':
-            return 20
-        elif mano[0][0] == 'Q':
-            return 20
-        elif mano[0][0] == 'K':
-            return 20
-        return 10 + mano[0][0]
-    elif mano[1][0] == 'Q':
-        if mano[0][0] == 'J':
-            return 20
-        elif mano[0][0] == 'Q':
-            return 20
-        elif mano[0][0] == 'K':
-            return 20
-        return 10 + mano[0][0]
-    elif mano[1][0] == 'K':
-        if mano[0][0] == 'J':
-            return 20
-        elif mano[0][0] == 'Q':
-            return 20
-        elif mano[0][0] == 'K':
-            return 20
-        return 10 + mano[0][0]
-
+    if mano == []:
+        return 0
     else:
-        return mano[0][0] + mano[1][0]
+        return valor_carta(mano[0]) + valor_mano(mano[1:])
+
+
+def hay_aces(mano):
+    if mano == []:
+        return False
+    else:
+        if str(mano[0][0]) == "A":
+            return True
+        else:
+            return hay_aces(mano[1:])
+
+
+def valor_aces(mano):
+    if mano == []:
+        return 0
+    else:
+        if hay_aces(mano) and valor_mano(mano) <= 10:
+            return 10 + valor_mano(mano)
+        else:
+            return valor_mano(mano)
+
+def verificar_mano(mano):
+        if hay_aces(mano) and valor_mano(mano) <= 21:
+            return True
+        else:
+            print "Perdio ome"
+            return False
 
 
 def jugar(mazo, casa, jugador):
-    print casa
+    print str(casa[1:])+"[(*, *)]"
     print jugador
-    if mazo != []:
-        if casa == []:
-            return jugar(mazo[4:], casa + mazo[:2], jugador + mazo[2:4])
-        if valor_mano(casa) < 21 and valor_mano(jugador) < 21:
-            print valor_mano(casa)
-            print valor_mano(jugador)
-            return jugar(mazo[2:], casa + [mazo[0]], jugador + [mazo[1]])
+    print "--------------------------------------------"
+    print valor_aces(casa)
+    print valor_aces(jugador)
+    print "--------------------------------------------"
+
+    if (jugador==[] and casa==[]):
+
+        return jugar(mazo[4:], casa + mazo[:2], jugador + mazo[2:4])
+
+
+    if (input( "Desea otra carta?:  si = 1/ no = 0:   ") == 1):
+        if mazo != []:
+            if jugador == []:
+                return jugar(mazo[4:],casa, jugador + mazo[2:4])
+            if valor_aces(jugador) < 21:
+                return jugar(mazo[2:], casa, jugador + [mazo[1]])
+    else:
+        if mazo != []:
+            if casa == []:
+                return jugar(mazo[4:], casa + mazo[:2], jugador)
+            if valor_aces(casa) < 21 :
+                return jugar(mazo[2:], casa + [mazo[0]], jugador)
+
 
 
 jugar(generar_mazo(), [], [])
